22b76d4d0573673357e091dee7b0bb197e531a87
GGsrvg <x@y.z>
2020-11-10 19:29:22 +0300

    Initial Commit

---
+++ LDS/LDS.h
@@ -0,0 +1,18 @@
+//
+//  LDS.h
+//  LDS
+//
+//  Created by GGsrvg on 10.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LDS.
+FOUNDATION_EXPORT double LDSVersionNumber;
+
+//! Project version string for LDS.
+FOUNDATION_EXPORT const unsigned char LDSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDS/PublicHeader.h>
+
+
