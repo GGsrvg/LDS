50d7fd824c771828b09ec63a46a535644c837171
GGsrvg <x@y.z>
2021-03-20 15:14:55 +0300

    add test

--- LDSTests/LDSTests.swift
+++ LDSTests/LDSTests.swift
@@ -9,25 +9,80 @@ import XCTest
 @testable import LDS
 
 class LDSTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testTableViewAdapter() throws {
+        let observable = ObservableDataSource<String, String, String>()
+        
+        let tableView = UITableView()
+        let adapter = UITableViewAdapter<String, String, String>(
+            tableView,
+            observableArray: observable
+        )
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        tableView.dataSource = adapter
+        observableTest(observable)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testCollectionViewViewAdapter() throws {
+        let observable = ObservableDataSource<String, String, String>()
+        
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        let adapter = UICollectionViewAdapter<String, String, String>(
+            collectionView,
+            observableArray: observable
+        )
+        collectionView.dataSource = adapter
+        observableTest(observable)
     }
-
+    
+    private func observableTest( _ observable: ObservableDataSource<String, String, String>) {
+        observable.addSection(
+            .init(
+                header: "",
+                rows: [],
+                footer: ""
+            )
+        )
+        
+        observable.insertSection(
+            .init(
+                header: "",
+                rows: [],
+                footer: ""
+            ),
+            at: 0
+        )
+        
+        observable.updateSection(
+            .init(
+                header: "",
+                rows: [],
+                footer: ""
+            ),
+            at: 1
+        )
+        
+        
+        observable.removeSection(at: 0)
+        
+        observable.header("", section: 0)
+        observable.footer("", section: 0)
+        
+        observable.addRow("", section: 0)
+        observable.insertRow("", section: 0, at: 0)
+        observable.updateRow("", section: 0, at: 0)
+        observable.remove(row: "")
+        observable.removeRow(section: 0, at: 0)
+        observable.clearRow(section: 0)
+        observable.clear()
+    }
 }
